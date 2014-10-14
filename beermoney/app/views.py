f39be3116110f69de62fa5d6e613fbebdcc426ce
Justin Simonelli <justin.simonelli@sherwin.com>
2014-10-13 23:16:35 -0400

    -Register checkins. -Might be a better way of getting this from the request but not sure yet...so let's hack around it! get that shit, display it to the page. next up, storing it to a database! --using a sqlite DB for the time being (since it's super simple), but def need to change that later when we become billionaires

--- beermoney/app/views.py
+++ beermoney/app/views.py
@@ -1,7 +1,26 @@
 from app import app
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "hello world!"
 
+@app.route('/checkin')
+@app.route('/checkin/')
+def checkIt():
+    return render_template('checkin.html',
+                           title="get drunk. save cash.")
+
+ #make this a GET request for now so we can be lazy and use query params
+@app.route('/checkin/drink', methods=['GET'])
+@app.route('/checkin/drink/', methods=['GET'])
+def saveCheckIn():
+    _location = request.args.get('location', '')
+    _drink = request.args.get('drink', '')
+    _price = request.args.get('price', '')
+
+    return render_template('checkinSuccess.html',
+                               location = _location,
+                               drink = _drink,
+                               cost = _price)
